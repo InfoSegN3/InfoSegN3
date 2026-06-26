9bf7b6fd35753b3cea67220b01c4d6376e52b5a4
OtavioSantanna <otavio.santana085@gmail.com>
2026-06-26 14:02:18 -0300

    Configurações de segurança: host, senha, user e porta do banco adcionados ao .env; além do .gitignore

--- backend/conector.php
+++ backend/conector.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$db = "infosegn3";
-$user = "root";
-$pass = "";
+$host = $_ENV['DB_HOST'];
+$db = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO(
